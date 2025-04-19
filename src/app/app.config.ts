bdffd994b6c827e2507620e1886e605ca3c06c35
Susanne Schütt <susanne.schuett@interone.de>
2025-04-19 14:09:57 +0200

    feat(produkltseite): added routes and removed initial angular code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter([
+      { path: '', component: ProductPageComponent },
+      { path: 'products/page/:page', component: ProductPageComponent },
+    ]),
+    provideHttpClient(),
+    FormsModule,
+  ],
 };
